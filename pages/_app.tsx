3beac7e9f5cdc8ed0c2aa2dabf4670a37e173f71
Onur Çağan <onurcagann@gmail.com>
2023-01-09 13:49:58 +0300

    deleted a return that ended up in the main component by mistake

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
-      return <Component {...pageProps} />
+      <Component {...pageProps} />
     </main>
   )
 }
