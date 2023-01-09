cdaee907d3cd12790758f4c912580593b858c6b8
Onur Çağan <onurcagann@gmail.com>
2023-01-09 18:10:25 +0300

    extracted types

---
+++ src/types/apiTypes.ts
@@ -0,0 +1,11 @@
+export type Event = {
+  id: number
+  name: string
+  date: string
+  time: string
+  location: string
+  tickets_available: number
+  ticket_price: number
+  created_at: string
+  updated_at: string
+}
