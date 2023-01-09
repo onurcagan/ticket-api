8b0ef470a41d17bc048a3b21ffb237635c1eba6c
Onur Çağan <onurcagann@gmail.com>
2023-01-09 13:44:14 +0300

    added next-font & changed font color on homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      return <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col justify-center items-center align-center h-screen">
-      <h1 className="mt-auto text-bold text-white text-5xl">Front-End of Ticket Api</h1>
-      <div className="my-12" />
+      <h1 className="mt-auto text-bold text-gray-400 text-5xl">Front-End of Ticket Api</h1>
+      <div className="my-8" />
       <img src="/loading.svg" className="w-12 h-12 animate-spin mb-auto" />
-      <p className=" text-white text-xs">just kidding, there's nothing loading, no front-end yet</p>
+      <p className=" text-gray-400 text-xs">just kidding, there's nothing loading, no front-end yet</p>
     </div>
   )
 }
