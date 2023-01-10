fcb4cf3a05be6b71d60a31d4e380055a299ba89a
Onur Çağan <onurcagann@gmail.com>
2023-01-10 18:01:01 +0300

    made changes so that the "front-end" is responsive, although there isn't much to it

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center align-center h-screen">
-      <h1 className="mt-auto text-bold text-gray-400 text-5xl">Front-End of Ticket Api</h1>
+    <div className="flex flex-col items-center justify-center h-screen align-center">
+      <h1 className="mt-auto text-2xl text-gray-400 sm:text-3xl md:text-5xl text-bold">Front-End of Ticket Api</h1>
       <div className="my-8" />
-      <img src="/loading.svg" className="w-12 h-12 animate-spin mb-auto" />
-      <p className=" text-gray-400 text-xs">just kidding, there's nothing loading, no front-end yet</p>
+      <img src="/loading.svg" className="w-8 h-8 mb-auto sm:w-12 sm:h-12 animate-spin" />
+      <p className="text-xs text-gray-400 ">just kidding, there's nothing loading, no front-end yet</p>
     </div>
   )
 }
